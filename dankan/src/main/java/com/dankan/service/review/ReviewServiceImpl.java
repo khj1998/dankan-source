7a35dffbb01d4752447089fdc489441ffc5e3406
kimhojin98@naver.com <kimhojin98@naver.com>
2023-07-25 01:53:25 +0900

    Feat : 주소/지번으로 리뷰 검색 추가

--- dankan/src/main/java/com/dankan/service/review/ReviewServiceImpl.java
+++ dankan/src/main/java/com/dankan/service/review/ReviewServiceImpl.java
@@ -109,7 +109,7 @@ public class ReviewServiceImpl implements ReviewService {
         List<ReviewSearchResponse> responseDtoList = new ArrayList<>();
         HashMap<String,List<RoomReview>> reviewHashMap = new HashMap<>();
 
-        List<RoomReview> roomReviewList = reviewRepository.findByBuildingName(buildingName); // 건물 이름으로
+        List<RoomReview> roomReviewList = reviewRepository.findByBuildingSearch(buildingName); // 건물 이름으로
 
         for (RoomReview roomReview : roomReviewList) {
             String roomBuildingName = roomReview.getAddress().split(" ")[4];
@@ -127,8 +127,8 @@ public class ReviewServiceImpl implements ReviewService {
             String address = hashMap.getValue().get(0).getAddress();
             String imgUrl = "";
 
-            if (roomRepository.findFirstByRoomAddress_Address(address).isPresent()) {
-                Room room = roomRepository.findFirstByRoomAddress_Address(address)
+            if (roomRepository.findByAddress(address,1L).isPresent()) {
+                Room room = roomRepository.findByAddress(address,1L)
                         .orElseThrow(() -> new RoomNotFoundException(address));
 
                 Image image = imageRepository.findMainImage(room.getRoomId(),0L)
@@ -141,11 +141,55 @@ public class ReviewServiceImpl implements ReviewService {
             responseDtoList.add(reviewSearchResponse);
         }
 
-
         responseDtoList.sort( //별점 순 조회
                 Comparator.comparing(ReviewSearchResponse::getAvgTotalRate).reversed()
         );
         
+        return responseDtoList;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<ReviewSearchResponse> findReviewByAddress(String address) {
+        List<ReviewSearchResponse> responseDtoList = new ArrayList<>();
+        HashMap<String,List<RoomReview>> reviewHashMap = new HashMap<>();
+
+        List<RoomReview> roomReviewList = reviewRepository.findByAddressSearch(address);
+
+        for (RoomReview roomReview : roomReviewList) {
+            String roomBuildingName = roomReview.getAddress().split(" ")[4];
+
+            if (reviewHashMap.containsKey(roomBuildingName)) {
+                reviewHashMap.get(roomBuildingName).add(roomReview);
+            } else {
+                List<RoomReview> newRoomList = new ArrayList<>();
+                newRoomList.add(roomReview);
+                reviewHashMap.put(roomBuildingName,newRoomList);
+            }
+        }
+
+        for (Map.Entry<String, List<RoomReview>> hashMap : reviewHashMap.entrySet()) {
+            String reviewAddress = hashMap.getValue().get(0).getAddress();
+            String imgUrl = "";
+
+            if (roomRepository.findByAddress(address,1L).isPresent()) {
+                Room room = roomRepository.findByAddress(reviewAddress,1L)
+                        .orElseThrow(() -> new RoomNotFoundException(address));
+
+                Image image = imageRepository.findMainImage(room.getRoomId(),0L)
+                        .orElseThrow(() -> new ImageNotFoundException(room.getRoomId()));
+
+                imgUrl = image.getImageUrl();
+            }
+
+            ReviewSearchResponse reviewSearchResponse = ReviewSearchResponse.of(hashMap.getValue(),imgUrl);
+            responseDtoList.add(reviewSearchResponse);
+        }
+
+        responseDtoList.sort( //별점 순 조회
+                Comparator.comparing(ReviewSearchResponse::getAvgTotalRate).reversed()
+        );
+
         return responseDtoList;
     }
 

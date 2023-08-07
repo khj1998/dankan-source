a77e29b8c8d529f6a851aa95e771daf6b23c3f80
Qkdrmf <75231376+Qkdrmf@users.noreply.github.com>
2023-08-07 23:37:38 +0900

    Feat: 정렬, 검색, 로직 수정 (#48)
    * Feat : 건물이름으로 검색
    
    * Feat : 정렬순 type 파라미터 제거, 정렬 높은 평점순으로 수정
    
    * Fix : hibernate-search-orm 종속성 제거
    
    * Refactor: chatting history using caching user
    
    * Feat : 주소/지번으로 리뷰 검색 추가
    
    * Fix: fix wrong logic (#47)
    
    ---------
    
    Co-authored-by: kimhojin98@naver.com <kimhojin98@naver.com>
    Co-authored-by: Quokka_khj <80220062+khj1998@users.noreply.github.com>

--- dankan/src/main/java/com/dankan/controller/UnivController.java
+++ dankan/src/main/java/com/dankan/controller/UnivController.java
@@ -1,7 +1,9 @@
 package com.dankan.controller;
 
 import com.dankan.dto.request.email.EmailCodeRequestDto;
+import com.dankan.dto.request.email.EmailRequestDto;
 import com.dankan.dto.response.univ.UnivListResponseDto;
+import com.dankan.service.email.EmailService;
 import com.dankan.service.email.EmailServiceImpl;
 import com.dankan.service.univ.UnivService;
 import io.swagger.annotations.Api;
@@ -20,6 +22,7 @@ import java.util.List;
 @AllArgsConstructor
 public class UnivController {
     private final UnivService univService;
+    private final EmailService emailService;
 
     @Operation(summary = "대학교 리스트 조회 api", description = "사용자 리스트 조회")
     @ApiResponses(
@@ -30,5 +33,29 @@ public class UnivController {
     @GetMapping("/info")
     public ResponseEntity<List<UnivListResponseDto>> getUnivInfo() {
         return ResponseEntity.ok(univService.findAll());
+    }
+
+    @Operation(summary = "대학교 인증 메일 발송 api", description = "대학교 인증 메일 발송")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "발송 환료"),
+                    @ApiResponse(responseCode = "403", description = "관리자 권한 없음"),
+            }
+    )
+    @PostMapping("/mail")
+    public ResponseEntity<String> mailConfirm(@RequestBody EmailRequestDto email) throws Exception {
+        return ResponseEntity.ok(emailService.sendSimpleMessage(email.getEmail()));
+    }
+
+    @Operation(summary = "대학교 인증 코드 확인 api")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "발송 환료"),
+                    @ApiResponse(responseCode = "403", description = "관리자 권한 없음"),
+            }
+    )
+    @PostMapping("/verify-code")
+    public ResponseEntity<Boolean> verifyEmailCode(@RequestBody EmailCodeRequestDto emailCodeRequestDto) {
+        return ResponseEntity.ok(emailService.verifyCode(emailCodeRequestDto));
     }
 }

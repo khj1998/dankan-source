658ef8747a468bf30e21f1e9c24b09ffdc1f4912
Quokka_khj <80220062+khj1998@users.noreply.github.com>
2023-08-08 16:04:47 +0900

    PostRepository 안쓰는 쿼리 제거
    PostRepository 안쓰는 쿼리 제거

--- dankan/src/main/java/com/dankan/repository/PostRepository.java
+++ dankan/src/main/java/com/dankan/repository/PostRepository.java
@@ -24,11 +24,4 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     @Query("select p from Post p where p.userId = :userId and "
         +"p.isShown = :isShown")
     List<Post> findByUserId(@Param("userId") Long userId,@Param("isShown") Boolean isShown , Pageable pageable);
-
-    @Query("select p from Post p where p.isShown = :isShown")
-    Slice<Post> findAllPost(@Param("isShown") Boolean isShown, Pageable pageable);
-
-    @Query("select p from Post p where p.isShown = :isShown and "
-          +"")
-    Slice<Post> findAllPostByAddress(@Param("isShown") Boolean isShown, @Param("address") String address, Pageable pageable);
 }

758ccc937763ae66f4c795a9d8b64f50f73301b9
kimhojin98@naver.com <kimhojin98@naver.com>
2023-09-15 19:06:50 +0900

    Feat : 단칸 도메인 교차출처허용

--- dankan/src/main/java/com/dankan/config/SecurityConfig.java
+++ dankan/src/main/java/com/dankan/config/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.addAllowedOrigin("https://dankan-api.com:443");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
         configuration.setAllowCredentials(true);
